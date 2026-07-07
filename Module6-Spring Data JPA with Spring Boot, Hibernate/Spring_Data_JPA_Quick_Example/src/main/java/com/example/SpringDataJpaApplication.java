49c5b84c20a5fc783b53fed46b5ff21e192fd538
Gartham Deepika <garthamdeepika2006@gmail.com>
2026-07-07 22:42:44 +0530

    Added Module 6 Exercises 1 and 2

---
+++ Module6-Spring Data JPA with Spring Boot, Hibernate/Spring_Data_JPA_Quick_Example/src/main/java/com/example/SpringDataJpaApplication.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class SpringDataJpaApplication {
+
+    public static void main(String[] args) {
+
+        System.out.println("Spring Data JPA Quick Example");
+
+    }
+}

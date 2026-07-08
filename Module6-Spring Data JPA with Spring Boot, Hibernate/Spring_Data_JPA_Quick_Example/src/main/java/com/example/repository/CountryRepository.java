f17579b56e805ce7df1287e4bc744f8bc34f990d
Gartham Deepika <garthamdeepika2006@gmail.com>
2026-07-08 19:13:55 +0530

    Delete Module6-Spring Data JPA with Spring Boot, Hibernate directory

--- Module6-Spring Data JPA with Spring Boot, Hibernate/Spring_Data_JPA_Quick_Example/src/main/java/com/example/repository/CountryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.repository;
-
-import com.example.model.Country;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CountryRepository extends JpaRepository<Country, String> {
-}
